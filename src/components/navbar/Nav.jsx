09fdde6c256d348818a5b2c495a0c7a052eb25f2
Kush  Patel <kush@MacBook-Pro.local>
2022-01-01 22:11:36 -0500

    added search bar and styling updates

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,18 +1,62 @@
 import React, { useState } from "react";
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Icon from '@mui/material/Icon';
-import Container from '@mui/material/Container';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import { styled, alpha } from '@mui/material/styles';
 import { Avatar, Button, Tooltip, Typography } from "@mui/material";
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Icon from '@mui/material/Icon';
+import InputBase from '@mui/material/InputBase';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Toolbar from '@mui/material/Toolbar';
 
-const screens = ["Home", "About", "Post", "Contact"];
+
+const screens = ["About", "Blog", "Contact"];
 const acountScreens = ["Profile", "Account", "Logout"]
 
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+            width: '20ch',
+        },
+        },
+    },
+}));
+  
 function Nav() {
     const [anchorNav, setAnchorNav] = useState(null)
     const [anchorUser, setAnchorUser] = useState(null)
@@ -34,19 +78,19 @@ function Nav() {
     }
 
     return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
+        <AppBar position="static" sx={{backgroundColor: "#3c4043"}}>
+            <Container maxWidth="l">
                 <Toolbar disableGutters>
                     <IconButton
                         size="large"
                         edge="start"
                         color="inherit"
                         aria-label="menu-item"
-                        sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+                        sx={{ mr: 0, display: { xs: "none", md: "flex" } }}
                     >
                         Logo
                     </IconButton>
-                    <Box sx={{ flexGrow: 1, display: {xs: "flex", md: "none"} }}>
+                    <Box sx={{ flexGrow: 0, display: {xs: "flex", md: "none"} }}>
                         <IconButton
                             size="large"
                             aria-label="user account menu"
@@ -82,14 +126,15 @@ function Nav() {
                             ))}
                         </Menu>
                     </Box>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
+                    <IconButton
+                        size="large"
+                        edge="end"
+                        color="inherit"
+                        aria-label="menu-item"
                         sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
                     >
-                        LOGO
-                    </Typography>
+                        Logo
+                    </IconButton>
                     <Box sx={{ flexGrow: 1, display: {xs: "none", md: "flex"} }}>
                         {screens.map((screen) => (
                             <Button
@@ -100,6 +145,22 @@ function Nav() {
                                 {screen}
                             </Button>
                         ))}
+                    </Box>
+                    <Box sx={{ flexGrow: 0, display: {xs: "none", md: "flex"}, pr: "24px" }}>
+                        <Search>
+                            <SearchIconWrapper>
+                                <Icon sx={{color: "white"}}>search</Icon>
+                            </SearchIconWrapper>
+                            <StyledInputBase
+                                placeholder="Search…"
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
+                        </Search>
+                    </Box>
+                    <Box sx={{ flexGrow: 0, display: {xs: "flex", md: "none"} }}>
+                        <IconButton size="large" aria-label="search" color="inherit">
+                            <Icon sx={{color: "white"}}>search</Icon>
+                        </IconButton>
                     </Box>
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Profile">
